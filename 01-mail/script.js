6bbdecdd13cbbfa14b94409f43ca7d64248141c2
mattiacoli <mattia.coli@aol.com>
2025-02-13 14:50:30 +0100

    first commit

---
+++ 01-mail/script.js
@@ -0,0 +1,18 @@
+/* TRACCIA
+
+  Crea una lista di email di invitati ad una festa.
+  Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+  stampa un messaggio appropriato sull’esito del controllo.
+
+*/
+
+/* Tools
+  
+  - array
+  - prompt
+  - loop for
+  - if / else
+  - console.log
+
+*/
+
