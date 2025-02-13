0c69c6a9fd9db6bebcfa7cab4b800d8cd1089fdc
mattiacoli <mattia.coli@aol.com>
2025-02-13 16:23:47 +0100

    add dadi excercise

--- 02-dadi/script.js
+++ 02-dadi/script.js
@@ -6,4 +6,35 @@ Prima di partire a scrivere codice poniamoci qualche domanda per individuare gli
 */
 
 
+/* Tools
+  
+  - Math.random
+  - if / else
+  - consol.log
+
+*/
+
+// scegliere un numero randomico per user
+const userNumb = Math.floor(Math.random()* 6 +1)
+console.log(userNumb);
+
+// scegliere numero randomico per il pc
+const pcNumb = Math.floor(Math.random()* 6 +1)
+console.log(pcNumb);
+
+//stabilire il vincitore in base a chi fa il punteggio piu` alto
+if (userNumb === pcNumb ){
+  console.log('Pareggio!');
+  
+} else if (userNumb > pcNumb){
+  console.log('Hai vinto!');
+  
+} else {
+  console.log('Hai perso , ritenta sarai piu fortunato');
+  
+}
+
+
+
+
 
