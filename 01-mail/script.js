87e23f79dbebac1885715636af1e56f083a332b8
mattiacoli <mattia.coli@aol.com>
2025-02-13 15:19:37 +0100

    add mail xcercise, to be completed

--- 01-mail/script.js
+++ 01-mail/script.js
@@ -16,3 +16,26 @@
 
 */
 
+// crea lista di email
+
+let mail = ['mattia@mail.com',
+  'fabio@mail.com',
+  'boolean@mail.com',
+  'giuseppe@mail.com',
+  'sarah@mail.com',
+  'giuliano@mail.com',
+  'fabiola@mail.com']
+
+// chiedere all'utente la sua mail
+let userMail = prompt('Inserisci la tua mail')
+let thisMail;
+
+
+// verificare che la sua mail sia presente nell'elenco
+for (let i = 0; i < mail.length; i++) {
+  thisMail = mail[i];
+  // stampa messaggio appropriato sull'esito
+  if (thisMail === userMail) {
+    alert('Congratulazioni sei tra gli invitati');
+  } 
+}
